8a1f87eff53f5d9157ee9ccb343cc20605b18261
nalchamp <nicholasalym@gmail.com>
2024-01-18 15:28:29 -0500

    Analyze and Explore the Climate Data complete

--- Surfsup/app.py
+++ Surfsup/app.py
@@ -1,11 +1,19 @@
 # Import the dependencies.
-
+from flask import Flask, jsonify
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import datetime as dt
 
 
 #################################################
 # Database Setup
 #################################################
 
+# Create engine to hawaii.sqlite
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
 
